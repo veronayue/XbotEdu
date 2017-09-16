9156071c45302ef99f21fa02996a5448f20fae71
veronayue <veronayue@gmail.com>
2017-09-16 14:51:37 -0700

    DriveToPosition Change on 9/16

--- EduBot/src/xbot/edubot/subsystems/drive/commands/DriveToPositionCommand.java
+++ EduBot/src/xbot/edubot/subsystems/drive/commands/DriveToPositionCommand.java
@@ -3,11 +3,20 @@ package xbot.edubot.subsystems.drive.commands;
 import com.google.inject.Inject;
 
 import xbot.common.command.BaseCommand;
+import xbot.common.controls.sensors.DistanceSensor;
 import xbot.edubot.subsystems.drive.DriveSubsystem;
 
 public class DriveToPositionCommand extends BaseCommand {
 
 	DriveSubsystem drive;
+	double goal;
+	double error;
+	boolean isErrorZero;
+	double oldPosition;
+	double velocity;
+	double errorVelocity;
+	double oldError;
+	
 	
 	@Inject
 	public DriveToPositionCommand(DriveSubsystem driveSubsystem) {
@@ -17,11 +26,15 @@ public class DriveToPositionCommand extends BaseCommand {
 	public void setTargetPosition(double position) {
 		// This method will be called by the test, and will give you a goal distance.
 		// You'll need to remember this target position and use it in your calculations.
+		goal= position;
 	}
 	
 	@Override
 	public void initialize() {
 		// If you have some one-time setup, do it here.
+
+		
+		//error
 	}
 
 	@Override
@@ -31,14 +44,65 @@ public class DriveToPositionCommand extends BaseCommand {
 		// - Hint: use drive.distanceSensor.get() to find out where you are
 		// - Gets the robot stop (or at least be moving really really slowly) at the target position
 		
+		//drive.tankDrive(1, 1);
+		error=goal-drive.distanceSensor.getDistance();
+		
+		/*if (error<0){
+			isErrorZero=true;
+			}
+			else{
+			isErrorZero=false;
+			}*/
+		
+		/*
+		if (error>0.28){
+				drive.tankDrive(1,1);	
+			}
+		if(error>0.0784 && error<=0.28){	
+		drive.tankDrive(0.3, 0.3);
+		}
+		if(error>0.0003 && error<=0.0784){	
+			drive.tankDrive(0.1, 0.1);
+			}
+			
+		if(error<0){
+		drive.tankDrive(-0.3, -0.3);
+		}*/
+	velocity=drive.distanceSensor.getDistance()-oldPosition;
+
+	errorVelocity=error-oldError;
+	
+		
+	double power=error*1+errorVelocity*5;
+	drive.tankDrive(power, power);
+	
+		
+		
+		/*while(error>0){
+			while (error>0.0784){
+				while (error>0.28){
+					drive.tankDrive(1, 1);
+				}
+				drive.tankDrive(0.5, 0.5);
+			}
+			drive.tankDrive(0, 0);
+		}*/
+			
+		//for(error=0.28;error>=0;error--);
+			
+			oldPosition=drive.distanceSensor.getDistance();
+			oldError=error;
+		}
+		
+				
 		// How you do this is up to you. If you get stuck, ask a mentor or student for some hints!
-	}
 	
 	@Override
 	public boolean isFinished() {
 		// Modify this to return true once you have met your goal, 
 		// and you're moving fairly slowly (ideally stopped)
-		return false;
+		return (Math.abs(error)<0.1 && Math.abs(velocity)<0.1);	
+		
 	}
 
 }
