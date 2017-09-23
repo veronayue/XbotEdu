212379366f25cb025c9d560d75955cdd8b3a5cf0
veronayue <veronayue@gmail.com>
2017-09-23 15:01:46 -0700

    TurnLeft90 First Part. but need to figure out the error1 compares to error2

--- EduBot/src/xbot/edubot/subsystems/drive/commands/TurnLeft90DegreesCommand.java
+++ EduBot/src/xbot/edubot/subsystems/drive/commands/TurnLeft90DegreesCommand.java
@@ -3,12 +3,24 @@ package xbot.edubot.subsystems.drive.commands;
 import com.google.inject.Inject;
 
 import xbot.common.command.BaseCommand;
+import xbot.edubot.rotation.MockHeadingSensor;
 import xbot.edubot.subsystems.drive.DriveSubsystem;
 
 public class TurnLeft90DegreesCommand extends BaseCommand {
 	
 	DriveSubsystem drive;
+	double startDegree;
+	double currentDegree;
+	double goalRotation;
+	double currentError;
+	double oldDegree;
+	double oldError;
+	double velocity;
+	double errorVelocity;
+	double error1;
+	double error2;
 	
+
 	@Inject
 	public TurnLeft90DegreesCommand(DriveSubsystem driveSubsystem) {
 		this.drive = driveSubsystem;
@@ -17,12 +29,45 @@ public class TurnLeft90DegreesCommand extends BaseCommand {
 	@Override
 	public void initialize() {
 		// TODO Auto-generated method stub
-		
+		startDegree=drive.gyro.getYaw();
 	}
 
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
+		
+		/*error1=goalRotation-drive.gyro.getYaw();
+		error2=360-error1;
+		if (Math.abs(error1)>Math.abs(error2)){
+			currentError=Math.abs(error2);
+		} else{
+			currentError=Math.abs(error1);
+		}*/
+		
+		goalRotation=startDegree+90;
+		if (Math.abs(goalRotation)>180){
+			goalRotation=goalRotation-360;
+		}
+		
+			currentError=Math.abs(goalRotation-currentDegree);
+		
+		
+		//velocity=drive.gyro.getYaw()-oldDegree;
+		errorVelocity=oldError-currentError;
+		
+		double power=currentError/13-errorVelocity/2;
+		/*if(Math.abs(power)>1){
+			power=1;
+		}*/
+		
+		drive.tankDrive(-power, power);
+		/*if (currentError);
+		{
+			
+		}*/
+		//oldDegree=drive.gyro.getYaw();
+		oldError=currentError;
+		currentDegree=drive.gyro.getYaw();
 	}
 
 
